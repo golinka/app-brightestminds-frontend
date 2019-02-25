8f1d37e7725be2e31a4245645556baa77239e6a6
Artem Holinka <artem.holinka@gmail.com>
2019-02-25 12:03:23 +0200

    feat(project): changed the eslint config and configured svg sprite

---
+++ src/layout/components/Icon.vue
@@ -0,0 +1,30 @@
+<template>
+  <svg :class="className" xmlns="http://www.w3.org/2000/svg">
+    <title v-if="title">{{ title }}</title>
+    <use :xlink:href="iconPath" xmlns:xlink="http://www.w3.org/1999/xlink"/>
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'icon',
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    title: {
+      type: String,
+      default: null
+    }
+  },
+  computed: {
+    iconPath () {
+      return require(`../../assets/sprite/${this.name}.svg`).default.url
+    },
+    className () {
+      return 'svg-icon svg-icon--' + this.name
+    }
+  }
+}
+</script>
---
+++ vue.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  lintOnSave: undefined,
+  pluginOptions: {
+    svgSprite: {
+      /*
+       * The directory containing your SVG files.
+       */
+      dir: 'src/assets/sprite',
+      /*
+       * The reqex that will be used for the Webpack rule.
+       */
+      test: /\.(svg)(\?.*)?$/,
+      /*
+       * @see https://github.com/kisenka/svg-sprite-loader#configuration
+       */
+      loaderOptions: {
+        extract: true,
+        spriteFilename: 'img/icons.[hash:8].svg' // or 'img/icons.svg' if filenameHashing == false
+      },
+      /*
+       * @see https://github.com/kisenka/svg-sprite-loader#configuration
+       */
+      pluginOptions: {
+        plainSprite: true
+      }
+    }
+  },
+  chainWebpack: config => {
+    config.module
+      .rule('svg-sprite')
+      .use('svgo-loader')
+      .loader('svgo-loader')
+  }
+}
