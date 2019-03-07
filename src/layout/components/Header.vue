52ca4b3b2ec0c85dae86d0e17d35c89f9f2058ba
golinka <artem.holinka@gmail.com>
2019-03-07 10:51:19 +0200

    chore(header): changed layout on mobile devices

--- src/layout/components/Header.vue
+++ src/layout/components/Header.vue
@@ -12,33 +12,35 @@
         <!-- LOGO END -->
         <!-- NAVIGATION -->
         <div class="header__navigation col-6 col-md-8" role="navigation">
-          <div class="row align-items-center justify-content-sm-end">
-            <div class="header__menu d-none d-md-flex flex-grow-1">
-              <ul class="justify-content-center align-items-baseline p-0">
-                <li><a href="https://brightestminds.io/">Home</a></li>
-                <li><a href="https://brightestminds.io/blog">Insights</a></li>
-                <li><a href="https://brightestminds.io/case-studies/">Case studies</a></li>
-                <li><a href="https://brightestminds.io/about-us/">About us</a></li>
-              </ul>
-            </div>
-            <div class="header__user d-flex align-items-center justify-content-end">
-              <template v-if="isAuth && user">
-                <Icon name="brain"/>
-                <span class="pl-2">
-                  {{ user.fname }} {{ user.lname }}
+          <div class="row">
+            <div class="col-12 d-flex align-items-center justify-content-end">
+              <div class="header__menu d-none d-md-flex flex-grow-1">
+                <ul class="justify-content-center align-items-baseline p-0">
+                  <li><a href="https://brightestminds.io/">Home</a></li>
+                  <li><a href="https://brightestminds.io/blog">Insights</a></li>
+                  <li><a href="https://brightestminds.io/case-studies/">Case studies</a></li>
+                  <li><a href="https://brightestminds.io/about-us/">About us</a></li>
+                </ul>
+              </div>
+              <div class="header__user d-flex align-items-center justify-content-end">
+                <template v-if="isAuth && user">
+                  <Icon name="brain"/>
+                  <span class="pl-2">
+                    {{ user.fname }} {{ user.lname }}
+                  </span>
+                </template>
+                <template v-else>
+                  <router-link tag="a" to="/login" class="ml-2">
+                    Login
+                  </router-link>
+                </template>
+              </div>
+              <div class="header__toggle ml-3">
+                <Icon name="menu-icon"/>
+                <span>
+                  <Icon name="close-icon"/>
                 </span>
-              </template>
-              <template v-else>
-                <router-link tag="a" to="/login" class="ml-2">
-                  Login
-                </router-link>
-              </template>
-            </div>
-            <div class="header__toggle ml-3">
-              <Icon name="menu-icon"/>
-              <span>
-                <Icon name="close-icon"/>
-              </span>
+              </div>
             </div>
           </div>
         </div>
--- src/layout/pages/Dashboard.vue
+++ src/layout/pages/Dashboard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="page products">
+  <div class="page mt-4 mt-sm-0">
     <div class="row">
       <div class="col-12">
         <h2 class="page__title">Dashboard</h2>
