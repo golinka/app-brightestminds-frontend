6f4813bcda688bd3b3113d99269eb34be399b4bd
golinka <artem.holinka@gmail.com>
2019-03-14 21:34:37 +0200

    feat(project): added vue-tables2 and vue-chartjs

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './layout/App.vue'
 import store from './store/store'
 import moment from 'vue-moment'
 import { router } from './router'
+import { ClientTable } from 'vue-tables-2'
 
 import Icon from './layout/components/Icon'
 
@@ -13,6 +14,7 @@ import "bootstrap/scss/bootstrap.scss"
 Vue.config.productionTip = false
 Vue.component('Icon', Icon)
 Vue.use(moment)
+Vue.use(ClientTable)
 
 new Vue({
   router,
