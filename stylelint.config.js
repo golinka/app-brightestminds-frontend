6befa05112940d12428a95af12e9b9ae981135f3
Артем Голинка <moyvebtop@gmail.com>
2019-07-28 12:36:00 +0300

    feat(project): added stylelint config

---
+++ stylelint.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-recess-order'
+  ],
+  rules: {
+    'no-empty-source': null,
+    'at-rule-no-unknown': null,
+    'no-descending-specificity': null
+  }
+}
